0db373daa3b03cd0020312bd79098c4a02f9dc66
Harold Blankenship <kithwood@gmail.com>
2016-11-23 15:18:07 -0600

    Adding resume files

---
+++ resume.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Harold L. Blankenship Resume</title>
+
+	<link rel="stylesheet" type="text/css" href="resume.css">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	
+	
+  </head>
+  <body class="dark">
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container half">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Project Name</a>
+			</div>
+ 			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#about">About</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</div>
+		</nav>
+	<div class="container half">						
+		<div  class="row">				
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<button type="button" class="prime"><span class="glyphicon glyphicon-user"></span></button>
+				<div class="stormy">
+					<label class="intro text-uppercase">Harold L. Blankenship</label><br/>
+					<label style="color:#fff;">Software Development Professional</label>
+				</div>
+				<button type="button" class="prime"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
+			</div>			
+			<div class="col-md-2"></div>
+		</div>
+	</div>
+  </body>
+</html>
+
