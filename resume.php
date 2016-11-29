cf549dfb3081d79b5307734e73c6aab7c253baf5
Kithwood <kithwood@gmail.com>
2016-11-28 21:24:56 -0600

    Test of Jenkins build

--- resume.php
+++ resume.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Harold L. Blankenship Resume</title>
+    <title>Harold L. Blankenship's Resume</title>
 
 	<link rel="stylesheet" type="text/css" href="resume.css">
 
